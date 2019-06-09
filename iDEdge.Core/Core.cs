a4593a494eb29ea65952521a28f2de2adbf39ea2
凌莞 <i@gao4.pw>
2019-06-09 15:03:52 +0800

    模块化

---
+++ iDEdge.Core/Core.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace iDEdge
+{
+    public class Core
+    {
+        public const string ver = "1.1.0";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请运行 iDEdge.exe 或 iDEdge-音乐平台.exe");
+        }
+
+
+        public static string Merge(string dir, string name)
+        {
+            Process merge = new Process();
+            merge.StartInfo.CreateNoWindow = false;
+            merge.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "\\MkvMerge.exe";
+            merge.StartInfo.UseShellExecute = false;
+            merge.StartInfo.Arguments = "--ui-language zh_CN " +
+                $"--output \"{Environment.CurrentDirectory}\\{name}.mkv\" " +
+                $"--language 0:eng ( \"{AppDomain.CurrentDomain.BaseDirectory}\\res.pak\" ) " +
+                $"--language 0:und ( \"{dir}mp3\" ) " +
+                $"--language 0:und ( \"{dir}lrc\" ) --track-order 0:0,1:0,2:0";
+            merge.StartInfo.RedirectStandardOutput = true;
+            merge.Start();
+            merge.WaitForExit();
+            string output = merge.StandardOutput.ReadToEnd();
+            merge.Close();
+            return output;
+        }
+
+        public static string Lrc2Ass(string lrc, string title)
+        {
+            Regex timeReg = new Regex(@"(?<=^\[)(\d|\:|\.)+(?=])");
+            Regex strReg = new Regex(@"(?<=]).+", RegexOptions.RightToLeft);
+            string[] lrcLines = lrc.Split('\n');
+            lrc = "[Script Info]\nTitle: Convented Sub\nScriptType: v4.00+\nWrapStyle: 0\nScaledBorderAndShadow: yes\nYCbCr Matrix: None\n\n[V4 + Styles]\nFormat: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\nStyle: Default,微软雅黑,30,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,2,8,10,10,10,1\n\n[Events]\nFormat: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\n";
+            TimeSpan last = TimeSpan.Zero;
+            string lastlrc = title;
+            foreach (string line in lrcLines)
+            {
+                if (line.Trim() == "")
+                    continue;
+                Match timeMatch = timeReg.Match(line);
+                if (!timeMatch.Success)
+                    continue;
+                Match strMatch = strReg.Match(line);
+                TimeSpan time = TimeSpan.Parse("00:" + timeMatch.Value);
+                if (lastlrc.Trim() != "")
+                    lrc += "Dialogue: 0," + string.Format("{0:d2}:{1:d2}:{2:d2}.{3:d2}", last.Hours, last.Minutes, last.Seconds, last.Milliseconds / 10) +
+                        "," + string.Format("{0:d2}:{1:d2}:{2:d2}.{3:d2}", time.Hours, time.Minutes, time.Seconds, time.Milliseconds / 10) +
+                        ",Default,,0,0,0,," + lastlrc.Trim() + "\n";
+                last = time;
+                lastlrc = strMatch.Success ? strMatch.Value : "";
+            }
+            TimeSpan timelast = last.Add(TimeSpan.FromSeconds(10));
+            if (lastlrc.Trim() != "")
+                lrc += "Dialogue: 0," + string.Format("{0:d2}:{1:d2}:{2:d2}.{3:d2}", last.Hours, last.Minutes, last.Seconds, last.Milliseconds / 10) +
+                    "," + string.Format("{0:d2}:{1:d2}:{2:d2}.{3:d2}", timelast.Hours, timelast.Minutes, timelast.Seconds, timelast.Milliseconds / 10) +
+                    ",Default,,0,0,0,," + lastlrc.Trim() + "\n";
+            return lrc;
+        }
+
+
+        public static string GetWebText(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream datastream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(datastream, Encoding.UTF8);
+            string result = reader.ReadToEnd();
+            reader.Close();
+            datastream.Close();
+            response.Close();
+            return result;
+        }
+    }
+}
---
+++ iDEdge.Netease/Nease.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace iDEdge.Netease
+{
+    class Nease
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"iDEdge {Core.ver}");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("用法:\n" +
+                    "iDEdge.exe [网易云链接|歌名]");
+                Environment.Exit(2);
+            }
+            string id = args[0];
+            Environment.Exit(Nease.Make(id));
+
+        }
+        public static int Make(string id)
+        {
+            if (id == null || id == "")
+                return 1;
+            string dir = Environment.GetEnvironmentVariable("temp") + "\\" + DateTime.Now.ToBinary().ToString() + "\\";
+            Directory.CreateDirectory(dir);
+            if (id.IndexOf("http") > -1)
+            {
+                Console.WriteLine("检测到分享链接");
+                id = Url2Id(id);
+            }
+            else
+            {
+                Console.WriteLine("可能是歌名搜索");
+                id = Name2Id(id);
+            }
+            string name = Id2Name(id);
+            Console.WriteLine(name);
+            IdDownMp3(id, dir + "mp3");
+            string lrc = Id2Lrc(id);
+            lrc = Core.Lrc2Ass(lrc, $"iDEdge {Core.ver} 生成的室内操");
+
+            File.WriteAllText(dir + "lrc", lrc, Encoding.UTF8);
+            string output = Core.Merge(dir, name);
+            if (File.Exists($"{Environment.CurrentDirectory}\\{name}.mkv"))
+                Console.WriteLine("成功");
+            else
+            {
+                Console.WriteLine("失败");
+                File.WriteAllText(dir + "log", output);
+                Console.WriteLine($"日志已保存到 {dir}");
+                return 4;
+            }
+            return 0;
+        }
+        public static string Id2Lrc(string id)
+        {
+            string lrcaddr = $"https://v1.itooi.cn/netease/lrc?id={id}";
+            return Core.GetWebText(lrcaddr);
+        }
+
+        public static void IdDownMp3(string id, string path)
+        {
+            string mp3 = $"https://v1.itooi.cn/netease/url?id={id}&quality=flac";
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(mp3, path);
+        }
+
+        public static string Id2Name(string id)
+        {
+            string nameaddr = $"https://v1.itooi.cn/netease/song?id={id}";
+            string name = Core.GetWebText(nameaddr);
+            return name.LastBetween("\"name\":\"", "\"");
+        }
+
+        public static string Name2Id(string id)
+        {
+            string url = $"https://v1.itooi.cn/netease/search?keyword={id}&type=song&pageSize=1";
+            string r = Core.GetWebText(url);
+            return r.Between("\"id\":", ",");
+        }
+
+        public static string Url2Id(string id)
+        {
+            if (id.IndexOf('?') > -1)
+            {
+                id = id.GetRight("id=");
+                if (id.IndexOf("&") > -1)
+                    id = id.GetLeft("&");
+            }
+            else if (id.IndexOf("/song/") > -1)
+            {
+                id = id.GetRight("/song/");
+                id = id.Trim('/');
+            }
+            return id;
+        }
+
+    }
+}
